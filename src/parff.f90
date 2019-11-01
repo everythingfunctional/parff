b6bb0ba108aaee5a63a420bbf6baf15d76eae015
Brad Richardson <brichardson@structint.com>
2019-11-01 12:24:08 -0700

    Implement dropThen function

--- src/parff.f90
+++ src/parff.f90
@@ -63,7 +63,7 @@ module parff
         end function thenParser
     end interface
 
-    public :: either, newState, parseChar, sequence
+    public :: dropThen, either, newState, parseChar, sequence
 contains
     function ConsumedOk(parsed, remaining, position, message_)
         class(ParsedValue_t), intent(in) :: parsed
@@ -79,6 +79,69 @@ contains
         ConsumedOk%position = position
         ConsumedOk%message = message_
     end function ConsumedOk
+
+    function dropThen(parser1, parser2, state_) result(result_)
+        procedure(parser) :: parser1
+        procedure(parser) :: parser2
+        type(State_t), intent(in) :: state_
+        type(ParseResult_t) :: result_
+
+        type(ParseResult_t) :: first_result
+
+        first_result = parser1(state_)
+        if (first_result%ok) then
+            result_ = parser2( &
+                    State(first_result%remaining, first_result%position))
+            if (.not.first_result%empty) then
+                result_%empty = .false.
+            end if
+        else
+            result_ = first_result
+        end if
+    end function dropThen
+
+    function either(parse1, parse2, state_) result(result_)
+        procedure(parser) :: parse1
+        procedure(parser) :: parse2
+        type(State_t), intent(in) :: state_
+        type(ParseResult_t) :: result_
+
+        type(ParseResult_t) :: first_result
+        type(ParseResult_t) :: second_result
+
+        first_result = parse1(state_)
+
+        if (first_result%empty) then
+            second_result = parse2(state_)
+            if (second_result%empty) then
+                if (first_result%ok) then
+                    result_ = mergeOk( &
+                            first_result%parsed, &
+                            first_result%remaining, &
+                            first_result%position, &
+                            first_result%message, &
+                            second_result%message)
+                else
+                    if (second_result%ok) then
+                        result_ = mergeOk( &
+                                second_result%parsed, &
+                                second_result%remaining, &
+                                second_result%position, &
+                                first_result%message, &
+                                second_result%message)
+                    else
+                        result_ = mergeError( &
+                                first_result%message, &
+                                second_result%message)
+                    end if
+                end if
+            else
+                result_ = second_result
+            end if
+        else
+            result_ = first_result
+        end if
+    end function either
 
     function EmptyError(message_)
         type(Message_t), intent(in) :: message_
@@ -113,6 +176,41 @@ contains
 
         new_message = Message(message_%position, message_%found, [label])
     end function expect
+
+    function merge_(message1, message2) result(merged)
+        type(Message_t), intent(in) :: message1
+        type(Message_t), intent(in) :: message2
+        type(Message_t) :: merged
+
+        merged = Message( &
+                message1%position, &
+                message1%found, &
+                [message1%expected, message2%expected])
+    end function merge_
+
+    function mergeError(message1, message2) result(result_)
+        type(Message_t), intent(in) :: message1
+        type(Message_t), intent(in) :: message2
+        type(ParseResult_t) :: result_
+
+        result_ = EmptyError(merge_(message1, message2))
+    end function mergeError
+
+    function mergeOk( &
+            parsed, remaining, position, message1, message2) result(result_)
+        class(ParsedValue_t), intent(in) :: parsed
+        type(VARYING_STRING), intent(in) :: remaining
+        type(Position_t), intent(in) :: position
+        type(Message_t), intent(in) :: message1
+        type(Message_t), intent(in) :: message2
+        type(ParseResult_t) :: result_
+
+        result_ = EmptyOk( &
+                parsed, &
+                remaining, &
+                position, &
+                merge_(message1, message2))
+    end function mergeOk
 
     function Message(position, found, expected)
         use iso_varying_string, only: VARYING_STRING
@@ -194,95 +292,6 @@ contains
         end function theMatcher
     end function parseChar
 
-    function State(input, position)
-        use iso_varying_string, only: VARYING_STRING
-
-        type(VARYING_STRING), intent(in) :: input
-        type(Position_t), intent(in) :: position
-        type(State_t) :: State
-
-        State%input = input
-        State%position = position
-    end function State
-
-    function either(parse1, parse2, state_) result(result_)
-        procedure(parser) :: parse1
-        procedure(parser) :: parse2
-        type(State_t), intent(in) :: state_
-        type(ParseResult_t) :: result_
-
-        type(ParseResult_t) :: first_result
-        type(ParseResult_t) :: second_result
-
-        first_result = parse1(state_)
-
-        if (first_result%empty) then
-            second_result = parse2(state_)
-            if (second_result%empty) then
-                if (first_result%ok) then
-                    result_ = mergeOk( &
-                            first_result%parsed, &
-                            first_result%remaining, &
-                            first_result%position, &
-                            first_result%message, &
-                            second_result%message)
-                else
-                    if (second_result%ok) then
-                        result_ = mergeOk( &
-                                second_result%parsed, &
-                                second_result%remaining, &
-                                second_result%position, &
-                                first_result%message, &
-                                second_result%message)
-                    else
-                        result_ = mergeError( &
-                                first_result%message, &
-                                second_result%message)
-                    end if
-                end if
-            else
-                result_ = second_result
-            end if
-        else
-            result_ = first_result
-        end if
-    end function either
-
-    function merge_(message1, message2) result(merged)
-        type(Message_t), intent(in) :: message1
-        type(Message_t), intent(in) :: message2
-        type(Message_t) :: merged
-
-        merged = Message( &
-                message1%position, &
-                message1%found, &
-                [message1%expected, message2%expected])
-    end function merge_
-
-    function mergeError(message1, message2) result(result_)
-        type(Message_t), intent(in) :: message1
-        type(Message_t), intent(in) :: message2
-        type(ParseResult_t) :: result_
-
-        result_ = EmptyError(merge_(message1, message2))
-    end function mergeError
-
-    function mergeOk( &
-            parsed, remaining, position, message1, message2) result(result_)
-        class(ParsedValue_t), intent(in) :: parsed
-        type(VARYING_STRING), intent(in) :: remaining
-        type(Position_t), intent(in) :: position
-        type(Message_t), intent(in) :: message1
-        type(Message_t), intent(in) :: message2
-        type(ParseResult_t) :: result_
-
-        result_ = EmptyOk( &
-                parsed, &
-                remaining, &
-                position, &
-                merge_(message1, message2))
-    end function mergeOk
-    !
     function satisfy(matches, state_) result(result_)
         use iso_varying_string, only: VARYING_STRING, len, var_str
         use strff, only: firstCharacter, withoutFirstCharacter
@@ -342,6 +351,17 @@ contains
             result_ = first_result
         end if
     end function sequence
+
+    function State(input, position)
+        use iso_varying_string, only: VARYING_STRING
+
+        type(VARYING_STRING), intent(in) :: input
+        type(Position_t), intent(in) :: position
+        type(State_t) :: State
+
+        State%input = input
+        State%position = position
+    end function State
 
     function withLabel(label, parse, state_) result(result_)
         use iso_varying_string, only: VARYING_STRING, char
--- tests/drop_then_test.f90
+++ tests/drop_then_test.f90
@@ -1,10 +1,10 @@
-module sequence_test
+module drop_then_test
     implicit none
     private
 
-    public :: test_sequence
+    public :: test_drop_then
 contains
-    function test_sequence() result(tests)
+    function test_drop_then() result(tests)
         use Vegetables_m, only: TestItem_t, Describe, It
 
         type(TestItem_t) :: tests
@@ -12,7 +12,7 @@ contains
         type(TestItem_t) :: individual_tests(3)
 
         individual_tests(1) = It( &
-                "When both parses pass, the results are combined", &
+                "When both parses pass, we get the results of the second one", &
                 checkBothPass)
         individual_tests(2) = It( &
                 "When the first parse fails, that error comes back", &
@@ -20,41 +20,41 @@ contains
         individual_tests(3) = It( &
                 "When the second parse fails, that error comes back", &
                 checkSecondFail)
-        tests = Describe("sequence", individual_tests)
-    end function test_sequence
+        tests = Describe("dropThen", individual_tests)
+    end function test_drop_then
 
     function checkBothPass() result(result_)
         use iso_varying_string, only: var_str
-        use parff, only: ParseResult_t, ParsedString_t, newState, sequence
+        use parff, only: ParseResult_t, ParsedCharacter_t, dropThen, newState
         use Vegetables_m, only: Result_t, assertEquals, assertThat, fail
 
         type(Result_t) :: result_
 
         type(ParseResult_t) :: parse_result
 
-        parse_result = sequence(parseA, thenParseB, newState(var_str("AB")))
+        parse_result = dropThen(parseA, parseB, newState(var_str("AB")))
 
         result_ = assertThat(parse_result%ok)
         if (result_%passed()) then
             select type (string => parse_result%parsed)
-            type is (ParsedString_t)
-                result_ = assertEquals("AB", string%value_)
+            type is (ParsedCharacter_t)
+                result_ = assertEquals("B", string%value_)
             class default
-                result_ = fail("Didn't get string back")
+                result_ = fail("Didn't get character back")
             end select
         end if
     end function checkBothPass
 
     function checkFirstFail() result(result_)
         use iso_varying_string, only: var_str
-        use parff, only: ParseResult_t, newState, sequence
+        use parff, only: ParseResult_t, dropThen, newState
         use Vegetables_m, only: Result_t, assertEquals, assertNot, fail
 
         type(Result_t) :: result_
 
         type(ParseResult_t) :: parse_result
 
-        parse_result = sequence(parseA, thenParseB, newState(var_str("BB")))
+        parse_result = dropThen(parseA, parseB, newState(var_str("BB")))
 
         result_ = assertNot(parse_result%ok)
         if (result_%passed()) then
@@ -66,14 +66,14 @@ contains
 
     function checkSecondFail() result(result_)
         use iso_varying_string, only: var_str
-        use parff, only: ParseResult_t, newState, sequence
+        use parff, only: ParseResult_t, dropThen, newState
         use Vegetables_m, only: Result_t, assertEquals, assertNot, fail
 
         type(Result_t) :: result_
 
         type(ParseResult_t) :: parse_result
 
-        parse_result = sequence(parseA, thenParseB, newState(var_str("AA")))
+        parse_result = dropThen(parseA, parseB, newState(var_str("AA")))
 
         result_ = assertNot(parse_result%ok)
         if (result_%passed()) then
@@ -92,34 +92,12 @@ contains
         result_ = parseChar("A", state_)
     end function parseA
 
-    function thenParseB(previous, state_) result(result_)
-        use iso_varying_string, only: assignment(=)
-        use parff, only: &
-                ParseResult_t, &
-                ParsedCharacter_t, &
-                ParsedString_t, &
-                ParsedValue_t, &
-                State_t, &
-                parseChar
+    function parseB(state_) result(result_)
+        use parff, only: ParseResult_t, State_t, parseChar
 
-        class(ParsedValue_t), intent(in) :: previous
         type(State_t), intent(in) :: state_
         type(ParseResult_t) :: result_
 
-        type(ParsedString_t) :: parsed
-
         result_ = parseChar("B", state_)
-
-        if (result_%ok) then
-            select type (previous)
-            type is (ParsedCharacter_t)
-                select type (next => result_%parsed)
-                type is (ParsedCharacter_t)
-                    parsed%value_ = previous%value_ // next%value_
-                end select
-            end select
-            deallocate(result_%parsed)
-            allocate(result_%parsed, source = parsed)
-        end if
-    end function thenParseB
-end module sequence_test
+    end function parseB
+end module drop_then_test
