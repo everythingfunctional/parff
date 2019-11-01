ff3b325f507e3bf3115b25bde6eac61ba3a069a4
Brad Richardson <brichardson@structint.com>
2019-11-01 10:41:15 -0700

    Finish testing sequence

--- tests/sequence_test.f90
+++ tests/sequence_test.f90
@@ -9,11 +9,17 @@ contains
 
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(1)
+        type(TestItem_t) :: individual_tests(3)
 
         individual_tests(1) = It( &
                 "When both parses pass, the results are combined", &
                 checkBothPass)
+        individual_tests(2) = It( &
+                "When the first parse fails, that error comes back", &
+                checkFirstFail)
+        individual_tests(3) = It( &
+                "When the second parse fails, that error comes back", &
+                checkSecondFail)
         tests = Describe("sequence", individual_tests)
     end function test_sequence
 
@@ -38,6 +44,44 @@ contains
             end select
         end if
     end function checkBothPass
+
+    function checkFirstFail() result(result_)
+        use iso_varying_string, only: var_str
+        use parff, only: ParseResult_t, ParsedString_t, newState, sequence
+        use Vegetables_m, only: Result_t, assertEquals, assertNot, fail
+
+        type(Result_t) :: result_
+
+        type(ParseResult_t) :: parse_result
+
+        parse_result = sequence(parseA, thenParseB, newState(var_str("BB")))
+
+        result_ = assertNot(parse_result%ok)
+        if (result_%passed()) then
+            result_ = &
+                    assertEquals("B", parse_result%message%found) &
+                    .and.assertEquals("A", parse_result%message%expected(1))
+        end if
+    end function checkFirstFail
+
+    function checkSecondFail() result(result_)
+        use iso_varying_string, only: var_str
+        use parff, only: ParseResult_t, ParsedString_t, newState, sequence
+        use Vegetables_m, only: Result_t, assertEquals, assertNot, fail
+
+        type(Result_t) :: result_
+
+        type(ParseResult_t) :: parse_result
+
+        parse_result = sequence(parseA, thenParseB, newState(var_str("AA")))
+
+        result_ = assertNot(parse_result%ok)
+        if (result_%passed()) then
+            result_ = &
+                    assertEquals("A", parse_result%message%found) &
+                    .and.assertEquals("B", parse_result%message%expected(1))
+        end if
+    end function checkSecondFail
 
     function parseA(state_) result(result_)
         use parff, only: ParseResult_t, State_t, charP
