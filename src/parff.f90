dc798ea0544ab309f4fc3e70f16bf1ac7a773d55
Brad Richardson <brichardson@structint.com>
2019-12-10 20:10:51 -0800

    Remove duplicated logic in many combinators

--- src/parff.f90
+++ src/parff.f90
@@ -283,50 +283,7 @@ contains
         type(State_t), intent(in) :: the_state
         type(ParserOutput_t) :: the_result
 
-        type(ParsedItems_t) :: start
-
-        allocate(start%items(0))
-        the_result = recurse(start, the_state)
-    contains
-        pure recursive function recurse(previous, state_) result(result_)
-            class(ParsedValue_t), intent(in) :: previous
-            type(State_t), intent(in) :: state_
-            type(ParserOutput_t) :: result_
-
-            type(ParsedItems_t) :: all
-            type(ParserOutput_t) :: next
-            type(ParsedItem_t) :: the_item
-
-            select type (previous)
-            type is (ParsedItems_t)
-                next = the_parser(state_)
-                if (next%ok) then
-                    allocate(the_item%item, source = next%parsed)
-                    allocate(all%items, source = [previous%items,the_item])
-                    result_ = recurse(all, State(next%remaining, next%position))
-                else
-                    if (size(previous%items) == 0) then
-                        result_ = EmptyOk( &
-                                previous, &
-                                state_%input, &
-                                state_%position, &
-                                Message( &
-                                        state_%position, &
-                                        var_str(""), &
-                                        [VARYING_STRING::]))
-                    else
-                        result_ = ConsumedOk( &
-                                previous, &
-                                state_%input, &
-                                state_%position, &
-                                Message( &
-                                        state_%position, &
-                                        var_str(""), &
-                                        [VARYING_STRING::]))
-                    end if
-                end if
-            end select
-        end function recurse
+        the_result = manyWithSeparator(the_parser, parseNothing, the_state)
     end function many
 
     pure function many1(the_parser, the_state) result(the_result)
@@ -334,25 +291,7 @@ contains
         type(State_t), intent(in) :: the_state
         type(ParserOutput_t) :: the_result
 
-        the_result = sequence(the_parser, more, the_state)
-    contains
-        pure function more(previous, state_) result(result_)
-            class(ParsedValue_t), intent(in) :: previous
-            type(State_t), intent(in) :: state_
-            type(ParserOutput_t) :: result_
-
-            type(ParsedItems_t) :: all
-            type(ParsedItem_t) :: first_item
-
-            allocate(first_item%item, source = previous)
-            result_ = many(the_parser, state_)
-            select type (items => result_%parsed)
-            type is (ParsedItems_t)
-                allocate(all%items, source = [first_item, items%items])
-                deallocate(result_%parsed)
-                allocate(result_%parsed, source = all)
-            end select
-        end function more
+        the_result = many1WithSeparator(the_parser, parseNothing, the_state)
     end function many1
 
     pure function many1WithSeparator( &
@@ -395,28 +334,9 @@ contains
         type(ParserOutput_t) :: the_result
 
         type(ParsedItems_t) :: all
-        type(ParserOutput_t) :: next
-        type(ParsedItems_t) :: temp
 
-        the_result = the_parser(the_state)
-        if (the_result%ok) then
-            allocate(all%items(1))
-            allocate(all%items(1)%item, source = the_result%parsed)
-            do
-                next = dropThen(the_separator, the_parser, State(the_result%remaining, the_result%position))
-                if (.not.next%ok) exit
-                allocate(temp%items(size(all%items)))
-                temp%items = all%items
-                deallocate(all%items)
-                allocate(all%items(size(temp%items) + 1))
-                all%items(1:size(temp%items)) = temp%items
-                allocate(all%items(size(all%items))%item, source = next%parsed)
-                deallocate(temp%items)
-                the_result = next
-            end do
-            deallocate(the_result%parsed)
-            allocate(the_result%parsed, source = all)
-        else
+        the_result = many1WithSeparator(the_parser, the_separator, the_state)
+        if (.not.the_result%ok) then
             allocate(all%items(0))
             the_result = EmptyOk( &
                     all, &
