3e836fbe894a4d3a1b1a6aa5c7f7d9be1e03b14c
Brad Richardson <everythingfunctional@protonmail.com>
2021-10-08 14:05:26 -0500

    refac(parsed_integer): expose component

--- src/parff/parsed_integer_m.f90
+++ src/parff/parsed_integer_m.f90
@@ -6,28 +6,6 @@ module parff_parsed_integer_m
     public :: parsed_integer_t
 
     type, extends(parsed_value_t) :: parsed_integer_t
-        private
-        integer :: value__
-    contains
-        private
-        procedure, public :: value_
-    end type
-
-    interface parsed_integer_t
-        module procedure constructor
-    end interface
-contains
-    pure function constructor(value_) result(parsed_integer)
-        integer, intent(in) :: value_
-        type(parsed_integer_t) :: parsed_integer
-
-        parsed_integer%value__ = value_
-    end function
-
-    pure function value_(self)
-        class(parsed_integer_t), intent(in) :: self
         integer :: value_
-
-        value_ = self%value__
-    end function
+    end type
 end module
